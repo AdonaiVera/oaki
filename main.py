52aedc47d763ce2dbc5c327f3a1fe29ad9935581
Adonai Vera <avera488@unab.edu.co>
2022-02-01 17:54:51 -0500

    Changes in develop

--- main.py
+++
@@ -1,7 +0,0 @@
-from oakiAgent import oaki
-
-oaki_cls = oaki()
-
-
-
-oaki_cls.run()
--- oakiAgent.py
+++ oakiAgent.py
@@ -14,10 +14,12 @@ from methods.types_of_exercise import TypeOfExercise
 ## General packages
 import imutils
 import time
+#import simpleaudio as sa
 from playsound import playsound
 import threading
 import numpy as np
-
+import multiprocessing
+from multiprocessing import Process
 
 class oaki():
     def __init__(self):
@@ -93,9 +95,27 @@ class oaki():
         self.exercise_type = 'push-up'
 
         # load Oaki
-        self.oaki_img = cv2.imread("img/oaki-happy.jpg", cv2.IMREAD_COLOR)
+        self.oaki_img = cv2.imread("img/OAKI.png", cv2.IMREAD_COLOR)
         self.bBandera = True
-        
+        self.process_count = 0
+        self.phrase = {
+            0: "Hi Welcome, my name is OAKI, and I'll be your trainer for today", 
+            1: "Let's start with some stretch",
+            2: "Go back! You have to be 3 meters from the camera",
+            3: "You know I can measure the depth from the camera to your body",
+            4: "Too much, please come closer",
+            5: "Now it's perfect, please put your hands up",
+            6: "Now put your hands forward",
+            7: "Now let's start the training",
+            8: "Today we are going to do some push-ups",
+            9: "Five more",
+            10: "The last one",
+            11: "Good Job, we are done for today",
+            12: "See you tomorrow Adonai",
+        }
+        self.q1 = multiprocessing.Queue()
+        p = Process(target=self.sound, args=(self.q1,))
+        p.start()
 
     def addImageWatermark(waterImg,OriImg,pos=(10,100),):
         tempImg = OriImg.copy()
@@ -107,9 +127,15 @@ class oaki():
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    def sound(self, nType):
-        path = "voice/{}.mp3".format(nType)
-        playsound(path)
+    def sound(self, queue):
+        time.sleep(5)
+        while True:
+            nCount = queue.get()
+
+            filename = "voice/{}.mp3".format(nCount)
+            playsound(filename)
+            time.sleep(2)
+
 
     def run(self):
         ## setup mediapipe
@@ -119,7 +145,19 @@ class oaki():
 
             # state of move
             status = True  
-
+            start_training = 0
+            bProcess0 = 0
+            bProcess1 = 0
+            bProcess2 = 0
+            bProcess3 = 0
+            bProcess4 = 0
+            bProcess5 = 0
+            bProcess6 = 0
+            bProcess7 = 0
+            bProcess8 = 0
+            bProcessX = 0
+            bProcessY = 0
+            bProcessW = 0
             # Connect to device and start self.pipeline
             with dai.Device(self.pipeline) as device:
 
@@ -132,6 +170,13 @@ class oaki():
                 color = (255, 255, 255)
 
                 while True:
+
+                    
+                    if self.bBandera:
+                        self.oaki_img_reduce = cv2.resize(self.oaki_img,(1280, 720))
+                        
+                        self.bBandera= False
+                    self.oaki_img_save = self.oaki_img_reduce.copy()
                     inDepth = depthQueue.get() # Blocking call, will wait until a new data has arrived
 
                     depthFrame = inDepth.getFrame()
@@ -144,6 +189,7 @@ class oaki():
                     ## make detection
                     results = pose.process(frame)
                     (imgH, imgW) = frame.shape[:2]
+
                     # Get the depth to the new object 
                     '''
                     config.roi = dai.Rect(topLeft, bottomRight)
@@ -156,16 +202,22 @@ class oaki():
                         counter, status = TypeOfExercise(landmarks).calculate_exercise(
                             self.exercise_type, counter, status)
 
-                        normalizedLandmark = detection_body_part(landmarks, "NOSE")
-                        
-                        pixelCoordinatesLandmarkTip = self.mp_drawing._normalized_to_pixel_coordinates(normalizedLandmark[0], normalizedLandmark[1], imgW, imgH)
+                        normalizedLandmarkRight = detection_body_part(landmarks, "RIGHT_HIP")
+                        normalizedLandmarkLeft = detection_body_part(landmarks, "LEFT_HIP")
+                        normalizedLandmarkShoulder = detection_body_part(landmarks, "LEFT_SHOULDER")
 
-                        topLx = pixelCoordinatesLandmarkTip[0]-10
-                        topLy = pixelCoordinatesLandmarkTip[1]-10
+                        pixelCoordinatesLandmarkRight = self.mp_drawing._normalized_to_pixel_coordinates(normalizedLandmarkRight[0], normalizedLandmarkRight[1], imgW, imgH)
+                        pixelCoordinatesLandmarkLeft = self.mp_drawing._normalized_to_pixel_coordinates(normalizedLandmarkShoulder[0], normalizedLandmarkShoulder[1], imgW, imgH)
+                        pixelCoordinatesLandmarkShoulder = self.mp_drawing._normalized_to_pixel_coordinates(normalizedLandmarkLeft[0], normalizedLandmarkLeft[1], imgW, imgH)
 
-                        bottomRx = pixelCoordinatesLandmarkTip[0]+10
-                        bottomRy = pixelCoordinatesLandmarkTip[1]+10
+
+                        topLx = normalizedLandmarkRight[0]
+                        topLy = normalizedLandmarkShoulder[1]
+
+                        bottomRx = normalizedLandmarkLeft[0]
+                        bottomRy = normalizedLandmarkLeft[1]
                         
+
                         # Optimize later
                         if topLx < 0:
                             topLx = 0
@@ -212,40 +264,129 @@ class oaki():
                         depthMin = depthData.depthMin
                         depthMax = depthData.depthMax
 
-                    
-                    img = np.zeros([int(frame.shape[0]/4),frame.shape[1]-100,3],dtype=np.uint8)
-                    img.fill(255) 
 
-                    xmin = 5
-                    ymin = 5
-                    xmax = 200
-                    ymax = 95
+                    # Dialogue Structure (I can improve this) | Sequential training. Simple v1
+                    if self.process_count == 0:
+                        self.process_count = 0
+                        if start_training == 0:
+                            self.q1.put(self.process_count)
+                        start_training += 1
+                    if self.process_count < 2 and start_training > 100:
+                        self.process_count = 1
+                        if bProcess0 == 0:
+                            self.q1.put(self.process_count)
+                        bProcess0 += 1
+                    if  float(depthData.spatialCoordinates.z/10) < 220 and self.process_count < 3 and bProcess0 > 100:
+
+                        self.process_count = 2
+
+                        if bProcessX == 0:
+                            self.q1.put(self.process_count)
+                        bProcessX += 1
+                    if  float(depthData.spatialCoordinates.z/10) < 220 and self.process_count < 3 and bProcessX > 100:
+                        self.process_count = 3
+
+                        if bProcessY == 0:
+                            self.q1.put(self.process_count)
+                        bProcessY += 1
+
+                    if self.process_count < 5 and float(depthData.spatialCoordinates.z/10) > 320:
+                        self.process_count = 4
+                        if bProcessW == 0:
+                            self.q1.put(self.process_count)
+                        bProcessW += 1
+                    if self.process_count < 6 and float(depthData.spatialCoordinates.z/10) < 320 and float(depthData.spatialCoordinates.z/10) > 220 and bProcessW > 50:
+                        self.process_count = 5
+                        if bProcess1 == 0:
+                            self.q1.put(self.process_count)
+                        bProcess1 += 1
+                    if self.process_count < 7 and counter < 5 and bProcess1 > 100:
+                        self.process_count = 6
+                        if bProcess2 == 0:
+                            self.q1.put(self.process_count)
+                        bProcess2 += 1
+                    if self.process_count < 8 and counter < 5 and bProcess2 > 100:
+                        self.process_count = 7
+                        if bProcess3 == 1:
+                            self.q1.put(self.process_count)
+                        bProcess3 += 1
+                        counter = 0
+                    if self.process_count < 9 and counter <5 and bProcess3 > 100:
+                        self.process_count = 8
+                        if bProcess4 == 0:
+                            self.q1.put(self.process_count)
+                        bProcess4 = 1
+                    if self.process_count < 10 and counter > 10:
+                        self.process_count = 9
+                        if bProcess5 == 0:
+                            self.q1.put(self.process_count)
+                        bProcess5 = 1
+                    if self.process_count < 11 and counter > 13:
+                        self.process_count = 10
+                        if bProcess6== 0:
+                            self.q1.put(self.process_count)
+                        bProcess6 += 1
+                    if self.process_count < 12 and counter > 14:
+                        self.process_count = 11
+                        if bProcess7 == 0:
+                            self.q1.put(self.process_count)
+                        bProcess7 += 1
+                    if self.process_count < 13 and bProcess7 > 200:
+                        self.process_count = 12
+                        if bProcess8 == 0:
+                            self.q1.put(self.process_count)
+                        bProcess8 = 1
+
+
+
+
+
+
+                    """
+                    self.phrase = {
+        
+                    2: "Go back! You have to be 3 meters from the camera",
+                    3: "You know I can measure the depth from the camera to your body",
+                    4: "Too much, please come closer",
+                    5: "Now it's perfect, please put your hands up",
+                    6: "Now put your hands forward",
+                    7: "Now let's start the training",
+                    8: "Today we are going to do some push-ups",
+                    9: "Five more",
+                    10: "The last one",
+                    11: "Good Job, we are done for today",
+                    12: "See you tomorrow Adonai",
+                }
+                    """
+
+
+
+                    y_offset = 50
+                    x_offset = 100
+                    self.oaki_img_save[y_offset:y_offset+frame.shape[0], x_offset:x_offset+frame.shape[1]] = frame
+                    
+                    xmin = 900
+                    ymin = 20
+
 
                     fontType = cv2.FONT_HERSHEY_TRIPLEX
-                    cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (0, 0, 0), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1)
-                    cv2.putText(img, f"Analytics Training", (xmin + 10, ymin + 15), fontType, 0.5, (0, 0, 0))
-                    cv2.putText(img, f"X: {int(depthData.spatialCoordinates.x/100)} cm", (xmin + 10, ymin + 30), fontType, 0.5, (0, 0, 0))
-                    cv2.putText(img, f"Y: {int(depthData.spatialCoordinates.y/100)} cm", (xmin + 10, ymin + 45), fontType, 0.5, (0, 0, 0))
-                    cv2.putText(img, f"Z: {int(depthData.spatialCoordinates.z/100)} cm", (xmin + 10, ymin + 60), fontType, 0.5, (0, 0, 0))
-                    cv2.putText(img, f"Repeticiones: {counter}", (xmin + 10, ymin + 75), fontType, 0.5, (0, 0, 0))
                     
-                    self.text = "tu puedes "
-                    text_print = "Good Job"
-                    img = cv2.putText(img, text_print, (int(frame.shape[1]/2), int(frame.shape[0]/8)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 1, cv2.LINE_AA)
-                    if self.bBandera:
-                        self.oaki_img_reduce = cv2.resize(self.oaki_img,(100, int(frame.shape[0]/4)))
-                        
-                        self.bBandera= False
+                    cv2.putText(self.oaki_img_save, f"Analytics Training", (xmin + 10, ymin + 30), fontType, 1, (255, 255, 255))
+                    cv2.putText(self.oaki_img_save, f"X: {int(depthData.spatialCoordinates.x/10)} cm", (xmin + 10, ymin + 60), fontType, 1, (255, 255, 255))
+                    cv2.putText(self.oaki_img_save, f"Y: {int(depthData.spatialCoordinates.y/10)} cm", (xmin + 10, ymin + 90), fontType, 1, (255, 255, 255))
+                    cv2.putText(self.oaki_img_save, f"Z: {int(depthData.spatialCoordinates.z/10)} cm", (xmin + 10, ymin + 120), fontType, 1, (255, 255, 255))
+                    cv2.putText(self.oaki_img_save, f"Repeticiones: {counter}", (xmin + 10, ymin + 150), fontType, 1, (255, 255, 255))
                     
-                    # we add the logo
-                    frame_logo = np.hstack((img,self.oaki_img_reduce))
-
-                    # Save the results in the image
-                    frame_results = np.vstack((frame,frame_logo))
                     
+                    text_print = self.phrase[self.process_count]
+                    img = cv2.putText(self.oaki_img_save, text_print, (int(self.oaki_img_save.shape[1]/25), int(self.oaki_img_save.shape[0]/1.25)), fontType, 0.7, (255, 255, 255))
+                    
+               
                     # Show the frame
-                    cv2.imshow("depth", frame_results)
-
+                    cv2.imshow("depth", self.oaki_img_save)
+                    
+                    
+                    
                     key = cv2.waitKey(1)
                     if key == ord('q'):
                         break
